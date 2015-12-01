9e7319f3152c345d9dffd9a619dbc992d65b9490
Anzu Mizuki <IshiAnzu_1918@hotmail.com>
2015-12-01 18:43:48 -0500

    Fuentes Espacio de Trabajo Sistema de Transporte

---
+++ src/com/co/sistematransporte/clases/MedioTransporte.java
@@ -0,0 +1,5 @@
+package com.co.sistematransporte.clases;
+
+public class MedioTransporte {
+
+}
---
+++ src/com/co/sistematransporte/controladores/STControl.java
@@ -0,0 +1,24 @@
+package com.co.sistematransporte.controladores;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class STControl {
+
+	public static void main(String[] args){
+		
+		try{
+		
+			Properties properties = new Properties();
+			properties.load(new FileInputStream("AppConfig" + File.separator + "inicio.properties"));
+		
+		} catch (Exception e){
+			
+			e.getMessage();
+			
+		}
+		
+	}
+	
+}
